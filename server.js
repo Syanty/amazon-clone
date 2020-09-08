8781f9ec4b9e93c16ddfb6137cbc3fc6b099ce97
syanty <santosh.tharuu@gmail.com>
2020-09-08 19:58:02 +0545

    creating safe environment for private details

--- server.js
+++ server.js
@@ -2,31 +2,36 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const app = express();
 const port = 3000;
 
-
-mongoose.connect('mongodb+srv://root:gOKOPiQwKyqswEIE@amazon-clone.cexmd.mongodb.net/<dbname>?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true }, err=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log('Mongodb connected')
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Mongodb connected");
     }
-})
+  }
+);
 
 //middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => res.json("Hello Amazon Clone"));
-app.post("/", (req,res)=> console.log(req.body.name))
+app.post("/", (req, res) => console.log(req.body.name));
 app.listen(port, (err) => {
-    if(err){
-        console.log(err)
-    }else{
-        console.log(`Amazon clone listening on port port!`)
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Amazon clone listening on port port!`);
+  }
 });
-
