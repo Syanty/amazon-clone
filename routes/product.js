e9e9d17e5bac277cf2e20f5ba5fcca4182ea05db
syanty <santosh.tharuu@gmail.com>
2020-09-11 08:13:19 +0545

    setting up multer and aws dependencies with upload-photo middleware in POST API

--- routes/product.js
+++ routes/product.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 const Product = require("../models/product");
-
+const upload = require("../middlewares/upload-photo");
 //POST ---- create product
 
-router.post("/products", async (req, res) => {
+router.post("/products", upload.single("photo"), async (req, res) => {
   try {
     let product = new Product();
-    product.title = req.body.title,
-    product.description = req.body.description,
-    product.photo = req.body.photo,
-    product.stockQuantity = req.body.stockQuantity,
-    await product.save();
+    (product.title = req.body.title),
+      (product.description = req.body.description),
+      (product.photo = req.file.location),
+      (product.stockQuantity = req.body.stockQuantity),
+      await product.save();
 
     res.json({
       status: true,
