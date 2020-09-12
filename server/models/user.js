3dacbe3d474ff097de2b476faed924ecd487dac0
syanty <santosh.tharuu@gmail.com>
2020-09-12 15:38:36 +0545

    Hashing password for user schema before saving

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bycrypt = require('bcrypt')
 
 const UserSchema = new Schema({
   name: String,
@@ -7,5 +8,27 @@ const UserSchema = new Schema({
   password: { type: String, required: true },
   address: { type: Schema.Types.ObjectId, ref: "Address" },
 });
+
+
+UserSchema.pre("save", function (next) {
+  let user = this
+  if(this.isModified("password") || this.isNew){
+    bycrypt.genSalt(10,function (err,salt) {
+      if(err){
+        return next(err)
+      }
+      bycrypt.hash(user.password, salt,null,function (err, hash) {
+        if(err){
+          return next(err)
+        }
+
+        user.password = hash
+        next()
+      })
+    })
+  }else{
+    return next()
+  }
+})
 
 module.exports = mongoose.model('User', UserSchema)
