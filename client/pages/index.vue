0f271997da1587775fb109be90ff1ccc25880d1a
syanty <santosh.tharuu@gmail.com>
2020-09-12 12:10:50 +0545

    FEATUREDPRODUCT container added

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="container">
-    
-  </div>
+  <main class="listingPage">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+          <!-- sidebar -->
+        </div>
+        <!-- main content -->
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <FeaturedProduct/>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {}
+import FeaturedProduct from '../components/FeaturedProduct'
+export default {
+  components:{
+    FeaturedProduct
+  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
