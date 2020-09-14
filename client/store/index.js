f25774df745f1674cf3c57d677ad1ba4bb2de207
syanty <santosh.tharuu@gmail.com>
2020-09-14 12:55:42 +0545

    persisting vuex data to localStorage

--- client/store/index.js
+++ client/store/index.js
@@ -42,7 +42,7 @@ export const actions = {
     if (!cartProduct) {
       commit("pushProductToCart", product);
     } else {
-      commit("incrementProductQty", product);
+      commit("incrementProductQty", cartProduct);
     }
     commit("incrementCartLength");
   }
